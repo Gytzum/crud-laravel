161669ad02fddf6a2106a9055c2ad077eddcec90
Gytzum <gytzum@gmail.com>
2021-08-10 20:18:10 +0300

    some fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,10 @@
     <div class="welcome">
         <h1 style="padding:15px; margin-top: 50px">Welcome to last sprint <br> "CRUD Application" </h1>
         <h2 style="padding:15px">This project is for learning purpose</h2>
-        <p>Start using application by registering new user</p>
+        <p style= "margin:0px">Start using as <Strong style="color:red">Reader</Strong>, register new user</p>
+        <p style= "margin:10px">Start using as <Strong style="color:red">Admin</Strong>, login with info given :</p>
+        <p style= "margin:0px">E-mail address: <Strong style="color:red">admin@admin.com</Strong> </p>
+        <p style= "margin:10px">Password: <Strong style="color:red">admin</Strong> </p>
         <ul class="welcome-ul">
             @if (Route::has('login'))
                 <li class="welcome-li">
